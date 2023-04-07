15640a6004c1421e1785c5ffaf042be373ca77f3
Hemal Maniar <41383071+hemal-maniar@users.noreply.github.com>
2023-04-06 21:37:10 -0500

    Add files via upload

---
+++ ToolInstallCfg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get install netdiscover
+apt-get install nmap
+apt-get install smbclient
+apt install metasploit-framework
+apt -y install exploitdb
+apt-get -y install nikto
+apt install dirbuster
+
